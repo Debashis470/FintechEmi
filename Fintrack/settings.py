c8ea2c39abdc45fc1c0e1cf538edfefeaaaab839
Debashis470 <debasisdhir7@gmail.com>
2025-10-01 21:03:14 +0530

    Update settings.py

--- Fintrack/settings.py
+++ Fintrack/settings.py
@@ -42,9 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'finance',
+   
     'rest_framework',
-    'finance1',
+    
     'finance2'
 
 ]
@@ -130,4 +130,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
