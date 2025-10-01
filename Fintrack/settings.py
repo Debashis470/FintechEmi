b21817c82f3d7febb96db49940974104bcb69072
Debashis470 <debasisdhir7@gmail.com>
2025-10-01 21:21:07 +0530

    Update settings.py

--- Fintrack/settings.py
+++ Fintrack/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'finance2',
+    'Fintrack',
 ]
 
 MIDDLEWARE = [
@@ -127,6 +128,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
 
 
 
